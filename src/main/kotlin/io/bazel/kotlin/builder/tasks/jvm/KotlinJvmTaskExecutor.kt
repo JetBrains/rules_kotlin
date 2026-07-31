076d519cf96447ac9f4d9c17b71fbec51af780f1
Eugene Zhuravlev <eugene.zhuravlev@jetbrains.com>
2026-07-31 19:56:33 +0200

    Introduce JvmTaskExecutor for pluggable compilation strategies
    - Introduce JvmTaskExecutor abstraction to allow legacy (KotlinJvmTaskExecutor) and BTA-based implementations (comes later)
    - Extract JvmCompilationTask-manipulation helpers that will be reused by the BTA JvmTaskExecutor implementation in a separate TaskArtifacts.kt file
    - CompilationTask.kt keeps helpers needed for the legacy KotlinJvmTaskExecutor implementation

--- src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
+++ src/main/kotlin/io/bazel/kotlin/builder/tasks/jvm/KotlinJvmTaskExecutor.kt
@@ -16,6 +16,7 @@
  */
 package io.bazel.kotlin.builder.tasks.jvm
 
+import io.bazel.kotlin.builder.tasks.JvmTaskExecutor
 import io.bazel.kotlin.builder.toolchain.CompilationStatusException
 import io.bazel.kotlin.builder.toolchain.CompilationTaskContext
 import io.bazel.kotlin.builder.toolchain.KotlinToolchain
@@ -30,7 +31,7 @@ const val X_FRIENDS_PATH_SEPARATOR = ","
 class KotlinJvmTaskExecutor(
   private val compilerBuilder: KotlinToolchain.KotlincInvokerBuilder,
   private val plugins: InternalCompilerPlugins,
-) {
+) : JvmTaskExecutor {
   private fun combine(
     one: Throwable?,
     two: Throwable?,
@@ -51,7 +52,7 @@ class KotlinJvmTaskExecutor(
     }
   }
 
-  fun execute(
+  override fun execute(
     context: CompilationTaskContext,
     task: JvmCompilationTask,
   ) {
